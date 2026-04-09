a6ec3e42797a4738a01d1e178a55fc2d90fca8b6
Hufevas <hufevas@gmail.com>
2026-04-09 10:23:37 -0300

    Add funcao remover

--- safevault_gui.py
+++ safevault_gui.py
@@ -63,6 +63,18 @@ def listar():
 
     messagebox.showinfo("Senhas", texto if texto else "Nenhuma senha")
 
+    def remover():
+    id_remover = entry_id.get()
+
+    if not id_remover:
+        messagebox.showerror("Erro", "Digite um ID válido")
+        return
+
+    cursor.execute("DELETE FROM credenciais WHERE id = ?", (id_remover,))
+    conn.commit()
+
+    messagebox.showinfo("Sucesso", "Senha removida com sucesso!")
+
 # ===== INTERFACE =====
 
 janela = tk.Tk()
